75e0dfc955bb846ef360d5bf61e03f9015f7af32
GParcade <ddann700@outlook.com>
2021-02-21 13:15:58 +0200

    FIX Now array fill random numbers

--- teamWork/teamWork.cpp
+++ teamWork/teamWork.cpp
@@ -7,12 +7,13 @@ int main()
 	int minRandVal = 123;
 	int maxRandVal = 500000;
 
-	int result = minRandVal + rand() % maxRandVal;
-
 	int arr[10] = { 2,34,3,2,4,42,2,3,4,37 };
 
 	for (size_t i = 0; i < 10; i++)
 	{
+
+		arr[i] = minRandVal + rand() % maxRandVal;
+
 		cout << arr[i] << endl;
 	}
 }
