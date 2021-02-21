33b22203281b38b0672ac19fe108f94afd060e16
Zahar44 <3axapduradashu@gmail.com>
2021-02-21 12:39:50 +0200

    #6 add config file

--- teamWork/teamWork.cpp
+++ teamWork/teamWork.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+	ifstream config("..\\teamWork\\config.txt");
+	if (!config.is_open())
+		throw new exception("config file not open");
+	
+	string initial_str;
+	getline(config, initial_str);
+	string confSize;
+	getline(config, confSize);
+	string end_str;
+	getline(config, end_str);
+
+	config.close();
+	
+	cout << initial_str << '\n';
+
+	size_t arrStartLen = stoi(confSize);
+
     int arr[10] = { 2,34,3,2,4,42,2,3,4,37 };
 
 	for (size_t i = 0; i < 10; i++)
 	{
 		cout << arr[i] << endl;
 	}
+
+	std::cout << end_str << '\n';
 }
