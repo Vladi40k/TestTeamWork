b1d2ecd2556fe7724b6dc402381319896776eade
GParcade <ddann700@outlook.com>
2021-02-21 12:59:47 +0200

    Random generator

--- teamWork/teamWork.cpp
+++ teamWork/teamWork.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+
+int random(int minRandVal, int maxRandVal) {
+	return minRandVal + rand() % maxRandVal;
+}
+
+
 int main()
 {
     int arr[10] = { 2,34,3,2,4,42,2,3,4,37 };
